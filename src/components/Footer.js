59371eb3bcea90e436203880d99db472b7137f3e
mikey <mike.owino40@yahoo.com>
2022-06-12 12:56:27 +0000

    ...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,11 @@ function Footer() {
                         <li><a href="aboutUs.html">About Us</a></li>
                         <li><a href="coffee.html">My Coffee</a></li>
                         <li><a href="contactUs.html">Contact us</a></li>
+                        {/* <Link to="/">Home</Link>
+                        <Link to="/AboutUs">About</Link>
+                        <Link to="/coffee">Coffee</Link>
+                        <Link to="/contactUs">Contact Us</Link> */}
+
                     </ul>
                 </nav>
             </footer>
