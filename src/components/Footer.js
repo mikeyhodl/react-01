2c13a20bd5090539915591e629da427eebe8db9f
mikey <mike.owino40@yahoo.com>
2022-06-12 12:57:59 +0000

    ....

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 function Footer() {
 
@@ -6,14 +8,14 @@ function Footer() {
             <footer>
                 <nav>
                     <ul>
-                        <li><a href="index.html">Home</a></li>
+                        {/* <li><a href="index.html">Home</a></li>
                         <li><a href="aboutUs.html">About Us</a></li>
                         <li><a href="coffee.html">My Coffee</a></li>
-                        <li><a href="contactUs.html">Contact us</a></li>
-                        {/* <Link to="/">Home</Link>
+                        <li><a href="contactUs.html">Contact us</a></li> */}
+                        <Link to="/">Home</Link>
                         <Link to="/AboutUs">About</Link>
                         <Link to="/coffee">Coffee</Link>
-                        <Link to="/contactUs">Contact Us</Link> */}
+                        <Link to="/contactUs">Contact Us</Link>
 
                     </ul>
                 </nav>
